d7dd3994a7fc23cec461252038a6aaf07c2374d0
Brian Brungardt <behrtarp@outlook.com>
2022-05-12 23:54:58 -0500

    Rearranged team formatting to reflect proper game sequences.

--- code/win_prob_4mat.py
+++ code/win_prob_4mat.py
@@ -18,7 +18,7 @@ df1 = df[0:15]
 
 list1 = df1.values.tolist()
 
-list1.insert(0, [1])
+list1.insert(1, [1])
 
 dfone = pd.DataFrame(list1)
 
@@ -31,7 +31,7 @@ df2 = df[15:30]
 
 list2 = df2.values.tolist()
 
-list2.insert(1, [1])
+list2.insert(7, [1])
 
 dftwo = pd.DataFrame(list2)
 
@@ -44,7 +44,7 @@ df3 = df[30:45]
 
 list3 = df3.values.tolist()
 
-list3.insert(2, [1])
+list3.insert(6, [1])
 
 dfthree = pd.DataFrame(list3)
 
@@ -57,7 +57,7 @@ df4 = df[45:60]
 
 list4 = df4.values.tolist()
 
-list4.insert(3, [1])
+list4.insert(5, [1])
 
 dffour = pd.DataFrame(list4)
 
@@ -70,7 +70,7 @@ df5 = df[60:75]
 
 list5 = df5.values.tolist()
 
-list5.insert(4, [1])
+list5.insert(10, [1])
 
 dffive = pd.DataFrame(list5)
 
@@ -83,7 +83,7 @@ df6 = df[75:90]
 
 list6 = df6.values.tolist()
 
-list6.insert(5, [1])
+list6.insert(13, [1])
 
 dfsix = pd.DataFrame(list6)
 
@@ -96,7 +96,7 @@ df7 = df[90:105]
 
 list7 = df7.values.tolist()
 
-list7.insert(6, [1])
+list7.insert(12, [1])
 
 dfseven = pd.DataFrame(list7)
 
@@ -109,7 +109,7 @@ df8 = df[105:120]
 
 list8 = df8.values.tolist()
 
-list8.insert(7, [1])
+list8.insert(14, [1])
 
 dfeight = pd.DataFrame(list8)
 
@@ -122,7 +122,7 @@ df9 = df[120:135]
 
 list9 = df9.values.tolist()
 
-list9.insert(8, [1])
+list9.insert(0, [1])
 
 dfnine = pd.DataFrame(list9)
 
@@ -135,7 +135,7 @@ df10 = df[135:150]
 
 list10 = df10.values.tolist()
 
-list10.insert(9, [1])
+list10.insert(4, [1])
 
 dften = pd.DataFrame(list10)
 
@@ -148,7 +148,7 @@ df11 = df[150:165]
 
 list11 = df11.values.tolist()
 
-list11.insert(10, [1])
+list11.insert(15, [1])
 
 dfeleven = pd.DataFrame(list11)
 
@@ -161,7 +161,7 @@ df12 = df[165:180]
 
 list12 = df12.values.tolist()
 
-list12.insert(11, [1])
+list12.insert(9, [1])
 
 dftwelve = pd.DataFrame(list12)
 
@@ -174,7 +174,7 @@ df13 = df[180:195]
 
 list13 = df13.values.tolist()
 
-list13.insert(12, [1])
+list13.insert(2, [1])
 
 dfthirteen = pd.DataFrame(list13)
 
@@ -187,7 +187,7 @@ df14 = df[195:210]
 
 list14 = df14.values.tolist()
 
-list14.insert(13, [1])
+list14.insert(8, [1])
 
 dffourteen = pd.DataFrame(list14)
 
@@ -200,7 +200,7 @@ df15 = df[210:225]
 
 list15 = df15.values.tolist()
 
-list15.insert(14, [1])
+list15.insert(3, [1])
 
 dffifteen = pd.DataFrame(list15)
 
@@ -213,13 +213,13 @@ df16 = df[225:240]
 
 list16 = df16.values.tolist()
 
-list16.insert(15, [1])
+list16.insert(11, [1])
 
 dfsixteen = pd.DataFrame(list16)
 
 # print (dfsixteen)
 
-merged_teams = pd.concat([dfone, dftwo, dfthree, dffour, dffive, dfsix, dfseven, dfeight, dfnine, dften, dfeleven, dftwelve, dfthirteen, dffourteen, dffifteen, dfsixteen],
+merged_teams = pd.concat([dfnine, dfone, dfthirteen, dffifteen, dften, dffour, dfthree, dftwo, dffourteen, dftwelve, dffive, dfsixteen, dfseven, dfsix, dfeight, dfeleven],
  axis=1, join="outer")
 
 # print(merged_teams)
